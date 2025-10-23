f8c539a91d0211d1c697f1634240f450174b25aa
Nelson Santos <nelsoncgsantos@gmail.com>
2025-10-23 16:09:58 -0600

    bugfixing around vite

--- api/resources/views/app.blade.php
+++ api/resources/views/app.blade.php
@@ -7,9 +7,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
 
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/main.tsx'])
-        @endif
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/main.tsx'])
     </head>
     <body class="antialiased bg-slate-950 text-slate-100">
         <div id="app" class="min-h-screen"></div>
